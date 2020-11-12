970dd06185d369660d63f2df2cb305162a4d8188
Administrative <bluebear1888@hotmail.com>
2020-11-12 17:00:47 +0900

    email updated1

--- app/Http/Controllers/WaitController.php
+++ app/Http/Controllers/WaitController.php
@@ -100,7 +100,7 @@ class WaitController extends Controller
         // }
 
 
-        Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+        Mailer::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
             {
                 $message->subject('Mailgun and Laravel are awesome!');
                 $message->from('no-reply@website_name.com', 'Website Name');
